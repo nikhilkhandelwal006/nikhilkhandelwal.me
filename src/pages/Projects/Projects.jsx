6e88b9e0c9a3301aad62a13fefb9d87af6469653
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2025-03-30 13:46:31 +0530

    Project page added

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Projects.css'
+import FD from "../../assets/FD.png"
 const Projects = () => {
   return (
     <div className='project-container'>
@@ -7,9 +8,9 @@ const Projects = () => {
             <h2>PROJECTS</h2>
         </div>
       <div className="showcase">
-        <div className="project-one"><video src=""></video></div>
-        <div className="project-two"></div>
-        <div className="project-three"></div>
+        <div className="project-one" ><img src={FD} alt="" /></div>
+        <div className="project-one"><img src={FD} alt="" /></div>
+        <div className="project-one"><img src={FD} alt="" /></div>
       </div>
     </div>
   )
