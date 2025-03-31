f426c8d6d8e528f59102b570ca2671c0dabd6549
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2025-03-31 16:34:57 +0530

    home button working

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import "./header.css";
 import wa from "../../assets/hm-1.jpg";
+import { useNavigate } from "react-router-dom";
 const Header = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/projects");
+  };
   return (
     <div className="header-container">
       <section className="image">
-      <div class="lines">
-        <div class="line"></div>
-        <div class="line"></div>
-        <div class="line"></div>
-        <div class="line"></div>
-        <div class="line"></div>
-    </div>
+        <div class="lines">
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+        </div>
         <img className="img-h" src={wa} alt="" />
         <div className="text-box">
-        
-        <span  className="overlay-text">Engineering Innovation with Code</span>
-<button className="view">View More</button>
+          <span className="overlay-text">Engineering Innovation with Code</span>
+          <button onClick={handleClick} className="view">
+            View More
+          </button>
         </div>
       </section>
     </div>
