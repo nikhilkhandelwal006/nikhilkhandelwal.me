3c210b45607d6a9940f66a0445be7d78ef1f84dd
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2025-04-01 16:31:32 +0530

    resume download button added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./header.css";
 import wa from "../../assets/hm-1.jpg";
-import { useNavigate } from "react-router-dom";
 const Header = () => {
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate("/projects");
-  };
+
+  // const handleClick = () => {
+  //   const resumeDownload =
+  //     "https://drive.google.com/uc?export=download&id=1ZOp0y5s71JSvynwfo8FR39X5YvgHUwnO";
+  //   window.location.href = resumeDownload;
+  // };
   return (
     <div className="header-container">
       <section className="image">
@@ -20,9 +21,12 @@ const Header = () => {
         <img className="img-h" src={wa} alt="" />
         <div className="text-box">
           <span className="overlay-text">Engineering Innovation with Code</span>
-          <button onClick={handleClick} className="view">
-            View More
-          </button>
+          {/* <button onClick={handleClick} className="view">
+            Download My Resume{" "}
+          </button> */}
+<a href="https://drive.google.com/uc?export=download&id=1ZOp0y5s71JSvynwfo8FR39X5YvgHUwnO" download>
+  <button  className="view">Download Resume</button>
+</a>
         </div>
       </section>
     </div>
