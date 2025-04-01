a421f42f14f72b5f336c42682784d78a736b4a54
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2025-04-01 23:13:56 +0530

    change project link

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -8,12 +8,12 @@ const Projects = () => {
             <h2>PROJECTS</h2>
         </div>
       <div className="showcase">
-        <div className="project-one" ><a href="https://github.com/nikhilkhandelwal006" target='_blank' rel="noopener noreferrer"><img src={FD} alt="Project Thumbnail" /></a></div>
-        <div className="project-one" ><a href="https://github.com/nikhilkhandelwal006" target='_blank' rel="noopener noreferrer"><img src={FD}  alt="Project Thumbnail" /></a></div>
-         <div className="project-one" ><a href="https://github.com/nikhilkhandelwal006" target='_blank' rel="noopener noreferrer"><img src={FD}  alt="Project Thumbnail" /></a></div>
-         <div className="project-one" ><a href="https://github.com/nikhilkhandelwal006" target='_blank' rel="noopener noreferrer"><img src={FD}  alt="Project Thumbnail" /></a></div>
-         <div className="project-one" ><a href="https://github.com/nikhilkhandelwal006" target='_blank' rel="noopener noreferrer"><img src={FD}  alt="Project Thumbnail" /></a></div>
-         <div className="project-one" ><a href="https://github.com/nikhilkhandelwal006" target='_blank' rel="noopener noreferrer"><img src={FD}  alt="Project Thumbnail" /></a></div>
+        <div className="project-one" ><a href="https://github.com/nikhilkhandelwal006/MERN-Projects" target='_blank' rel="noopener noreferrer"><img src={FD} alt="Project Thumbnail" /></a></div>
+        <div className="project-one" ><a href="https://github.com/nikhilkhandelwal006/MERN-Projects" target='_blank' rel="noopener noreferrer"><img src={FD}  alt="Project Thumbnail" /></a></div>
+         <div className="project-one" ><a href="https://github.com/nikhilkhandelwal006/MERN-Projects" target='_blank' rel="noopener noreferrer"><img src={FD}  alt="Project Thumbnail" /></a></div>
+         <div className="project-one" ><a href="https://github.com/nikhilkhandelwal006/MERN-Projects" target='_blank' rel="noopener noreferrer"><img src={FD}  alt="Project Thumbnail" /></a></div>
+         <div className="project-one" ><a href="https://github.com/nikhilkhandelwal006/MERN-Projects" target='_blank' rel="noopener noreferrer"><img src={FD}  alt="Project Thumbnail" /></a></div>
+         <div className="project-one" ><a href="https://github.com/nikhilkhandelwal006/MERN-Projects" target='_blank' rel="noopener noreferrer"><img src={FD}  alt="Project Thumbnail" /></a></div>
 
       </div>
     </div>
