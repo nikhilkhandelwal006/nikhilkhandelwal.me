951704d03977d7d7a3ad8cda3d6478e720a56734
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2025-04-04 12:08:32 +0530

    experience page working

--- src/pages/Experience/Experience.jsx
+++ src/pages/Experience/Experience.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import "./Experience.css"
+
 
 const Experience = () => {
   return (
     <div>
-      Hello
+     <section className="experience">
+      <div className="content">
+        <h1>My Experience</h1>
+        <ul>
+          <li>
+            <div className="experience-content">
+              <h2>Software Engineer</h2>
+              <p>Worked on full-stack development...</p>
+            </div>
+          </li>
+          <li>
+            <div className="experience-content">
+              <h2>Frontend Developer</h2>
+              <p>Focused on React, Tailwind CSS...</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
     </div>
   )
 }
